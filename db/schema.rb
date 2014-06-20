d9bf340c02c018ef3195e3f1fefcae45631ba93a
Colin Williams <lackita@gmail.com>
2014-06-19 20:02:54 -0400

    increase code_sample text size

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609232738) do
+ActiveRecord::Schema.define(version: 20140620000134) do
 
   create_table "code_samples", force: true do |t|
-    t.string   "text"
+    t.text     "text",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
