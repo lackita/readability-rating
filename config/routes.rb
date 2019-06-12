9671a1b0717d8e68be1c56447276b0db6d8ba3eb
Colin Williams <lackita@gmail.com>
2019-06-12 14:27:22 -0400

    Somebody put up a bunch of spam code samples, disabling creation of new ones.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ReadabilityRating::Application.routes.draw do
   root 'application#random'
-  resources 'code_samples' do
+  resources 'code_samples', except: ['new', 'create'] do
     resources 'ratings'
   end
 end
